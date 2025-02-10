1c9fc41900a6083f3f53fb7f21ff6390abc74d4a
Quaid Bartolomei <59486980+QuaidBartolomei@users.noreply.github.com>
2025-02-10 14:55:28 -0800

    unchecked save

--- src/types.ts
+++ src/types.ts
@@ -12,17 +12,11 @@ export interface useLinkedInType {
   state?: string;
   scope?: string;
   closePopupMessage?: string;
-  Browser?: {
-    open?: (options: { url: string }) => Promise<void>;
-    close?: () => void;
-  };
-}
-
-export interface useLinkedInIOSType extends useLinkedInType {
   Browser: {
     open?: (options: { url: string }) => Promise<void>;
     close?: () => void;
   };
+  isIOS?: boolean;
 }
 
 export interface LinkedInType extends useLinkedInType {
